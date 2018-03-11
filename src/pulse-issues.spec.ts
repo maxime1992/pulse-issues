502cd99ec62d06ad396e05ea661d21c7634b27d4
Maxime Robert <maxime.robert1992@gmail.com>
2018-03-11 19:27:34 +0000

    chore: add basic test

---
+++ src/pulse-issues.spec.ts
@@ -0,0 +1,8 @@
+import { PulseIssueProject } from './pulse-issues-project';
+
+describe('Pulse Issues', () => {
+  it('TODO', () => {
+    // TODO
+    expect(PulseIssueProject).toBeTruthy();
+  });
+});
